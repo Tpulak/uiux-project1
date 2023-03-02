6909df764b2760dfec2022ff41661ea94f301005
Tpulak <73133766+Tpulak@users.noreply.github.com>
2023-03-02 13:48:31 -0500

    set up food page and added images

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,16 +1,35 @@
-//import './Food.css';
+import './css/Food.css';
+import nathan from './img/nathan.webp'
+import rubys from './img/rubys.jpg'
+import cones from './img/cones.jpg'
 
 function Food() {
     return (
-      <div className="content">
-          This page will have information about the food.
-          <ul>
-            Nathans Famous
-            Coneys Cones
-            Rubys 
-            Williams Candy
-          </ul>
-          <h1>TESTING BRANCH TESTING</h1>
+      <div className="main">
+      <div className='header'>
+        <h1>Food</h1>
+        <h3>Eat the best food Coney Islan has to offer!</h3>
+      </div>
+      <div className='boxes'>
+        <div className='nathan'>
+          <img src={nathan} alt = "Nathans" />
+          <p>
+            DESRIPTION FOR Nathans
+          </p>
+        </div>
+        <div className='rubys'>
+          <img src={rubys} alt = "Rubys" />
+          <p>
+            DESSCRIPTION FOR RUBYS
+          </p>
+        </div>
+        <div className='cones'>
+          <img src={cones} alt="coneys cones" />
+          <p>
+            DESSCRIPTION FOR CONEYS CONES
+          </p>
+        </div>      
+      </div>
       </div>
     );
   }
